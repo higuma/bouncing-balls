e0c7474d1f34814a956a08ad2a6a8af386765dac
Yuji Miyane <myuj1964@gmail.com>
2014-09-09 15:16:09 +0900

    arrange non-overlapped random circles

--- public/js/application.js
+++ public/js/application.js
@@ -1,12 +1,117 @@
 (function() {
-  var canvas, dc;
+  var BALL_R_MAX, BALL_R_MIN, BALL_V_MAX, Ball, BallContainer, HEIGHT, N_BALLS, TWOPI, WIDTH, balls, canvas, dc, getRand, i, random, _i;
+
+  random = Math.random;
+
+  TWOPI = Math.PI * 2;
+
+  getRand = function(min, max) {
+    return random() * (max - min) + min;
+  };
+
+  WIDTH = 640;
+
+  HEIGHT = 480;
+
+  N_BALLS = 20;
+
+  BALL_R_MIN = 5;
+
+  BALL_R_MAX = 50;
+
+  BALL_V_MAX = 10;
+
+  Ball = (function() {
+    function Ball(owner) {
+      var _ref;
+      this.owner = owner;
+      this.r = getRand(BALL_R_MIN, BALL_R_MAX);
+      _ref = this.owner.getSpace(this.r), this.x = _ref[0], this.y = _ref[1];
+      this.vx = getRand(-BALL_V_MAX, BALL_V_MAX);
+      this.vy = getRand(-BALL_V_MAX, BALL_V_MAX);
+      this.owner.addBall(this);
+      return;
+    }
+
+    Ball.prototype.draw = function(dc) {
+      dc.beginPath();
+      dc.fillStyle = '#aaa';
+      dc.arc(this.x, this.y, this.r, 0, TWOPI);
+      dc.fill();
+      dc.closePath();
+      return this;
+    };
+
+    return Ball;
+
+  })();
+
+  BallContainer = (function() {
+    function BallContainer() {
+      this.balls = [];
+      this;
+    }
+
+    BallContainer.prototype.addBall = function(ball) {
+      this.balls.push(ball);
+      return this;
+    };
+
+    BallContainer.prototype.draw = function(dc) {
+      var ball, _i, _len, _ref;
+      _ref = this.balls;
+      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+        ball = _ref[_i];
+        ball.draw(dc);
+      }
+      return this;
+    };
+
+    BallContainer.prototype.isntOverlap = function(x, y, r) {
+      var ball, dx, dy, rr, _i, _len, _ref;
+      _ref = this.balls;
+      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+        ball = _ref[_i];
+        rr = r + ball.r;
+        dx = x - ball.x;
+        dy = y - ball.y;
+        if (dx * dx + dy * dy < rr * rr) {
+          return false;
+        }
+      }
+      return true;
+    };
+
+    BallContainer.prototype.getSpace = function(r) {
+      var i, x, y, _i;
+      for (i = _i = 0; _i < 1000; i = ++_i) {
+        x = getRand(r, WIDTH - r);
+        y = getRand(r, HEIGHT - r);
+        if (this.isntOverlap(x, y, r)) {
+          return [x, y];
+        }
+      }
+      throw "Cannot keep space";
+    };
+
+    return BallContainer;
+
+  })();
 
   canvas = document.getElementById('canvas');
 
   dc = canvas.getContext('2d');
 
-  dc.fillStyle = '#ccc';
+  dc.fillStyle = '#fafafa';
 
-  dc.fillRect(0, 0, 400, 400);
+  dc.fillRect(0, 0, WIDTH, HEIGHT);
+
+  balls = new BallContainer;
+
+  for (i = _i = 0; 0 <= N_BALLS ? _i < N_BALLS : _i > N_BALLS; i = 0 <= N_BALLS ? ++_i : --_i) {
+    new Ball(balls);
+  }
+
+  balls.draw(dc);
 
 }).call(this);
--- src/coffee/application.coffee
+++ src/coffee/application.coffee
@@ -1,4 +1,74 @@
+# utility functions
+
+random = Math.random
+TWOPI = Math.PI * 2
+
+getRand = (min, max) ->
+  random() * (max - min) + min
+
+WIDTH = 640
+HEIGHT = 480
+
+N_BALLS = 20
+BALL_R_MIN = 5
+BALL_R_MAX = 50
+BALL_V_MAX = 10
+
+class Ball
+  constructor: (@owner) ->
+    @r = getRand BALL_R_MIN, BALL_R_MAX
+    [@x, @y] = @owner.getSpace @r
+    @vx = getRand -BALL_V_MAX, BALL_V_MAX
+    @vy = getRand -BALL_V_MAX, BALL_V_MAX
+    @owner.addBall @
+    return
+
+  draw: (dc) ->
+    dc.beginPath()
+    dc.fillStyle = '#aaa'
+    dc.arc @x, @y, @r, 0, TWOPI
+    dc.fill()
+    dc.closePath()
+    @
+
+class BallContainer
+  constructor: ->
+    @balls = []
+    @
+
+  addBall: (ball) ->
+    @balls.push ball
+    @
+
+  draw: (dc) ->
+    ball.draw dc for ball in @balls
+    @
+
+  isntOverlap: (x, y, r) ->
+    for ball in @balls
+      rr = r + ball.r
+      dx = x - ball.x
+      dy = y - ball.y
+      return false if dx * dx + dy * dy < rr * rr
+    true
+
+  getSpace: (r) ->
+    for i in [0...1000]
+      x = getRand r, WIDTH - r
+      y = getRand r, HEIGHT - r
+      return [x, y] if @isntOverlap x, y, r
+    throw "Cannot keep space"
+
 canvas = document.getElementById 'canvas'
 dc = canvas.getContext '2d'
-dc.fillStyle = '#ccc'
-dc.fillRect 0, 0, 400, 400
+
+dc.fillStyle = '#fafafa'
+dc.fillRect 0, 0, WIDTH, HEIGHT
+
+balls = new BallContainer
+new Ball balls for i in [0...N_BALLS]
+
+balls.draw(dc)
+
+
+
